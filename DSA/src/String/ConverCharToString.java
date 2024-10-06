0a778f38fceb96a95afb3ea6ff3c3c5cc69cfc2a
mahesh8497 <maheshmtakale97@gmail.com>
2024-10-06 20:29:11 +0530

    add

---
+++ DSA/src/String/ConverCharToString.java
@@ -0,0 +1,14 @@
+package String;
+
+public class ConverCharToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[]ch= {'a','s','d'};
+		String s1=new String(ch);
+		System.out.println(s1);
+
+
+	}
+
+}
