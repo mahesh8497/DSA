28205207e11225442c7501b4fcba9551d0c06847
mahesh8497 <maheshmtakale97@gmail.com>
2024-10-06 20:35:45 +0530

    add

---
+++ DSA/src/String/q2.java
@@ -0,0 +1,14 @@
+package String;
+
+public class q2 {
+//Write a program to check whether a given string ends
+	//with the contents of another string.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Mahesh";
+		String s1="sh";
+		System.out.println(s.endsWith(s1));
+
+	}
+
+}
