662c9e6b8188331254e8d64d5dd1b9e7c87e6548
mahesh8497 <maheshmtakale97@gmail.com>
2024-10-06 20:41:55 +0530

    add

---
+++ DSA/src/String/Question_3.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Question_3 {
+//Write a program to check whether two String objects contain the same data 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="abcd";
+		String s1=new String("abcd");
+		System.out.println(s.equals(s1));//true
+		System.out.println(s==s1);//false
+
+	}
+
+}
